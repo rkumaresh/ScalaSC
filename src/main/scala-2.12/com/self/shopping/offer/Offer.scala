51d309075258d3f732d30a179039713e1e343ec3
rkumaresh <rkumaresh@hotmail.com>
2017-01-11 14:37:39 +0000

    Changed to Unit type

--- src/main/scala-2.12/com/self/shopping/offer/Offer.scala
+++ src/main/scala-2.12/com/self/shopping/offer/Offer.scala
@@ -5,7 +5,7 @@ import com.self.shopping.Item
 /**
   * Created by rangak on 10/01/2017.
   */
-abstract class Offer(offerItems : List[Item]) {
+abstract class Offer(unit: Unit) {
 
   def calculatePrice(itemList : List[Item]) : BigDecimal
 }
